c1a9f2ed94c1faa8befda6ae0ea58382b7cf76a2
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-03-16 08:12:25 +0300

    fix prev

--- lib/src/models/config.dart
+++ lib/src/models/config.dart
@@ -2,19 +2,19 @@ import 'package:meta/meta.dart';
 
 const cyclomaticComplexityDefaultWarningLevel = 20;
 const linesOfCodeDefaultWarningLevel = 50;
-const numberOfParametersDefaultWarningLevel = 4;
+const numberOfArgumentsDefaultWarningLevel = 4;
 
 /// Reporter config to use with various [Reporter]s
 @immutable
 class Config {
   final int cyclomaticComplexityWarningLevel;
   final int linesOfCodeWarningLevel;
-  final int numberOfParametersWarningLevel;
+  final int numberOfArgumentsWarningLevel;
 
   const Config(
       {this.cyclomaticComplexityWarningLevel =
           cyclomaticComplexityDefaultWarningLevel,
       this.linesOfCodeWarningLevel = linesOfCodeDefaultWarningLevel,
-      this.numberOfParametersWarningLevel =
-          numberOfParametersDefaultWarningLevel});
+      this.numberOfArgumentsWarningLevel =
+          numberOfArgumentsDefaultWarningLevel});
 }
